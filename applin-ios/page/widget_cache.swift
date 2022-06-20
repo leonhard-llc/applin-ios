ee5e2297fb68a2e2ae5b0dba9bde58f4fb007a42
Michael Leonhard <michael@leonhardllc.com>
2022-06-19 22:37:38 -0700

    Clarify WidgetCache's internal enum.

--- applin-ios/page/widget_cache.swift
+++ applin-ios/page/widget_cache.swift
@@ -1,20 +1,19 @@
-enum OptWidget {
-    case disabled
-    // Rename to `some`.
-    case present(WidgetProto)
+private enum CacheEntry {
+    case tombstone
+    case ok(WidgetProto)
 }
 
 class WidgetCache {
-    private var widgets: [String: OptWidget] = [:]
-    private var nextWidgets: [String: OptWidget] = [:]
+    private var widgets: [String: CacheEntry] = [:]
+    private var nextWidgets: [String: CacheEntry] = [:]
     private var scroll: [ScrollWidget] = []
     private var nextScroll: [ScrollWidget] = []
 
     public func remove(_ key: String) -> WidgetProto? {
         switch self.widgets.removeValue(forKey: key) {
-        case nil, .disabled:
+        case nil, .tombstone:
             return nil
-        case let .present(widget):
+        case let .ok(widget):
             for key in widget.keys() {
                 self.widgets.removeValue(forKey: key)
             }
@@ -34,11 +33,11 @@ class WidgetCache {
     public func putNext(_ widget: WidgetProto) {
         for key in widget.keys() {
             if self.nextWidgets[key] == nil {
-                self.nextWidgets[key] = .present(widget)
+                self.nextWidgets[key] = .ok(widget)
             } else {
                 // Two widgets use this key.  Don't use it for any widgets.
                 // We cannot use `self.widgets[key] = nil` since that deletes the entry.
-                self.nextWidgets.updateValue(.disabled, forKey: key)
+                self.nextWidgets.updateValue(.tombstone, forKey: key)
             }
         }
     }
