36f63c09a577c0ecd838b52c26fb71ee8f758cd2
Michael Leonhard <michael@leonhardllc.com>
2023-07-14 11:46:18 -0700

    Delete unused spinner.gif and spinner.swift.

--- applin-ios/widget/spinner.swift
+++
@@ -1,9 +0,0 @@
-//import Foundation
-//
-//struct ApplinSpinner: Equatable, Hashable {
-//    static let TYP = "spinner"
-//
-////    var body: some View {
-////        ProgressView()
-////    }
-//}
