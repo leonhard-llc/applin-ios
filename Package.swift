e5c919a1e3aeac93350fd82214d4692c8ca3d423
Michael Leonhard <michael@leonhardllc.com>
2023-08-15 12:46:08 -0700

    Support iOS 15 or newer.  This includes the iPhone 6S, released in 2015.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "ApplinIos",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v15),
     ],
     products: [
         .library(name: "ApplinIos", targets: ["ApplinIos"]),
